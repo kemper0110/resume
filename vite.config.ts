ad51fa5587c7eee565c2a77362364bde8f3dc5e9
kemper0110 <danil2003.2043@gmail.com>
2023-11-10 21:53:21 +0300

    + alias

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ import image from 'vite-image'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/resume/",
+  resolve: {
+    alias: {
+      "@": "/src"
+    }
+  },
   plugins: [react({
     babel: {
       plugins: [
