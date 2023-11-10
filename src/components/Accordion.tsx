a6197fee50a05d85842b4a0fbd19f156983e1b9e
kemper0110 <danil2003.2043@gmail.com>
2023-11-10 23:03:18 +0300

    accoc

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -1,25 +1,24 @@
 import {ReactNode, useId, useState} from "react";
 import downOpenIcon from '../assets/downOpen.svg';
 import downClosedIcon from '../assets/downClosed.svg';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import tw from "twin.macro";
 
 export const Accordion = ({label, children}: { label: ReactNode, children: ReactNode | ReactNode[] }) => {
     const [open, setOpen] = useState(false)
     const id = useId()
     return (
-        <div className="w-full overflow-hidden pt-3">
-            <input className="absolute opacity-0 peer" id={id} type="checkbox"
+        <div tw="w-full overflow-hidden pt-3">
+            <input tw="absolute opacity-0 peer" id={id} type="checkbox"
                    onChange={e => setOpen(e.target.checked)}/>
-            <label className={"flex md:flex-row flex-col-reverse gap-2 md:gap-8 border border-slate-600 rounded-lg items-center p-2 md:p-5 leading-normal cursor-pointer select-none"}
+            <label tw={"flex md:flex-row flex-col-reverse gap-2 md:gap-8 border border-slate-600 rounded-lg items-center p-2 md:p-5 leading-normal cursor-pointer select-none"}
                    htmlFor={id}>
-                {
-                    open ?
-                        <img className={'dark:invert'} src={downOpenIcon} alt={'down'}/> :
-                        <img className={'dark:invert'} src={downClosedIcon} alt={'down'}/>
-                }
+                <img tw={'dark:invert'} src={open ? downOpenIcon : downClosedIcon} alt={'down'}/>
                 {label}
             </label>
-            <div className="grid transition-all overflow-hidden grid-rows-[0fr] peer-checked:grid-rows-[1fr]">
-                <div className={'min-h-0 border-l pl-3 dark:bg-gray-800 border-slate-600 leading-normal'}>
+            <div tw="grid transition-all overflow-hidden grid-rows-[0fr] peer-checked:grid-rows-[1fr]">
+                <div tw={'min-h-0 border-l pl-3 dark:bg-gray-800 border-slate-600 leading-normal'}>
                     {children}
                 </div>
             </div>
