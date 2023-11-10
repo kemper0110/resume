16366d21d978ee53ba518bcaf37111218a235d9b
kemper0110 <danil2003.2043@gmail.com>
2023-11-10 21:34:36 +0300

    + twin.macro

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import image from 'vite-image'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/resume/",
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: [
+        'babel-plugin-macros',
+        [
+          '@emotion/babel-plugin-jsx-pragmatic',
+          {
+            export: 'jsx',
+            import: '__cssprop',
+            module: '@emotion/react',
+          },
+        ],
+        [
+          '@babel/plugin-transform-react-jsx',
+          { pragma: '__cssprop' },
+          'twin.macro',
+        ],
+      ]
+    }
+  }), image()],
 })
