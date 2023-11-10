b990c4bb077c776cdae4ae1286d12c973bca282e
kemper0110 <danil2003.2043@gmail.com>
2023-11-10 21:54:03 +0300

    migrate 2 - edu

--- src/blocks/table/Education.tsx
+++ src/blocks/table/Education.tsx
@@ -1,34 +1,33 @@
-import DatePeriod from "../../components/DatePeriod.tsx";
 import {TableContent, TableHeader} from "./Table.tsx";
+import {CalendarIcon, DatePeriod} from "@/components/DatePeriod.tsx";
 
 export const Education = () => {
     return (
         <tr>
-            <TableHeader title={'Образование'}/>
+            <TableHeader>
+                Образование
+            </TableHeader>
             <TableContent>
-                <DGTU/>
+                <div tw={'flex flex-wrap justify-between'}>
+                    <h2 tw={'font-medium text-2xl'}>Донской Государственный Технический Университет</h2>
+                    <DatePeriod>
+                        <CalendarIcon/>
+                        <time dateTime={'2020-09-01'}>
+                            Сент 2020
+                        </time>
+                        <span>-</span>
+                        <time dateTime={'2024-06-01'}>
+                            Июнь 2024
+                        </time>
+                    </DatePeriod>
+                </div>
                 <p>
                     Бакалавриат - Программная инженерия
                 </p>
-                <p className={'text-sm font-thin dark:text-slate-400 text-slate-500'}>
+                <p tw={'text-sm font-thin dark:text-slate-400 text-slate-500'}>
                     4 курс, средний балл - 5.0
                 </p>
             </TableContent>
         </tr>
     );
 };
-
-const DGTU = () => (
-    <div className={'flex flex-wrap justify-between'}>
-        <h2 className={'font-medium text-2xl'}>Донской Государственный Технический Университет</h2>
-        <DatePeriod dateFrom={
-            <time dateTime={'2020-09-01'} className={''}>
-                Сент 2020
-            </time>
-        } dateTo={
-            <time dateTime={'2024-06-01'} className={''}>
-                Июнь 2024
-            </time>
-        }/>
-    </div>
-)
