2f64c0ca536c0b082c754ad1e01bd666104ce6c5
kemper0110 <danil2003.2043@gmail.com>
2023-11-10 22:11:19 +0300

    migrate 2 - exp

--- src/blocks/table/Experience.tsx
+++ src/blocks/table/Experience.tsx
@@ -1,170 +1,155 @@
 import {TfiCup} from "react-icons/tfi";
-import SimpleDate from "../../components/SimpleDate.tsx";
 import {Item} from "../../components/Item.tsx";
 import {BsFire} from "react-icons/bs";
 import {Accordion} from "../../components/Accordion.tsx";
 import {ReactNode} from "react";
 import {TableContent, TableHeader} from "./Table.tsx";
+import tw from "twin.macro";
+import {CalendarIcon} from "@/components/DatePeriod.tsx";
 
 const Experience = () => {
     return (
         <tr>
-            <TableHeader title={'Опыт работы'}/>
+            <TableHeader>
+                Опыт работы
+            </TableHeader>
             <TableContent>
-                <HackathonWebant/>
-                <HackathonRostelecom/>
-                <HackathonTTK/>
+                <CompanyWrapper>
+                    <TitleWrapper title={'День карьеры ДГТУ 2023 - Вебант'}>
+                        <SimpleDate>
+                            <CalendarIcon/>
+                            <time dateTime={'2021-03'}>Март 2023</time>
+                        </SimpleDate>
+                    </TitleWrapper>
+                    <ItemContainer>
+                        <Item>React</Item>
+                        <Item>NodeJS</Item>
+                        <Item>ExpressJS</Item>
+                        <Item>PostgreSQL</Item>
+                        <Item>Chrome extension</Item>
+                    </ItemContainer>
+                    <Accordion label={
+                            <AccordionLabelWrapper>
+                            <span>
+                                Разработал веб-приложение для парсинга резюме с сайта <a href={'https://hh.ru'}
+                                                                                 tw='text-blue-400 underline'>hh.ru</a>,
+                                реализовал дополнительные возможности по улучшению пользовательского опыта.
+                            </span>
+                                <Cup title={'Победитель'}/>
+                            </AccordionLabelWrapper>
+                        }
+                    >
+                        <AccordionContentWrapper>
+                            <span>Выполненные требования: </span>
+                            <ul tw={'list-disc ms-5'}>
+                                <li>парсинг основных атрибутов по ссылке на резюме;</li>
+                                <li>сохранение в базу данных после проверки пользователем;</li>
+                                <li>веб-интерфейс для CRUD операций над резюме.</li>
+                            </ul>
+                            <span>Дополнительно реализовано: </span>
+                            <ul tw={'list-none ms-5'}>
+                                <li>
+                                    <BsFire tw={'inline'}/>
+                                    chrome extension content script - встраивает в страницу резюме кнопку для парсинга
+                                </li>
+                                <li>
+                                    <BsFire tw={'inline'}/>
+                                    chrome extension popup script - добавляет в браузер popup окно с возможностью
+                                    моментального
+                                    парсинга прямо из браузера
+                                </li>
+                            </ul>
+                        </AccordionContentWrapper>
+                    </Accordion>
+                </CompanyWrapper>
+
+
+                <CompanyWrapper>
+                    <TitleWrapper title={'Хакатон 2023 - Ростелеком'}>
+                        <SimpleDate>
+                            <CalendarIcon/>
+                            <time dateTime={'2021-03'}>Март 2023</time>
+                        </SimpleDate>
+                    </TitleWrapper>
+                    <ItemContainer>
+                        <Item>React</Item>
+                        <Item>XLSX</Item>
+                    </ItemContainer>
+                    <Accordion label={
+                        <AccordionLabelWrapper>
+                <span>
+                    Разработал веб-приложение для работы с таблицами Excel внутри браузера.
+                </span>
+                            <Cup title={'Финалист'}/>
+                        </AccordionLabelWrapper>
+                    }>
+                        <AccordionContentWrapper>
+                            <span>Выполненные требования: </span>
+                            <ul tw={'list-disc ms-5'}>
+                                <li>поиск заявок на подключение к сети по имени или ИНН клиента;</li>
+                                <li>обнаружение "запоздавших" по времени исполнения заявок (SLA);</li>
+                                <li>программная реализация в виде веб-приложения;</li>
+                                <li>возможность работы в оффлайн;</li>
+                                <li>дальнейшая возможность подключения к внутреннему REST API компании.</li>
+                            </ul>
+                        </AccordionContentWrapper>
+                    </Accordion>
+                </CompanyWrapper>
+
+
+                <CompanyWrapper>
+                    <TitleWrapper title={'Хакатон 2021 - ТТК'}>
+                        <SimpleDate>
+                            <CalendarIcon/>
+                            <time dateTime={'2021-03'}>Март 2021</time>
+                        </SimpleDate>
+                    </TitleWrapper>
+                    <ItemContainer>
+                        <Item>Python</Item>
+                        <Item>Selenium</Item>
+                        <Item>PyQT5</Item>
+                    </ItemContainer>
+                    <Accordion label={
+                        <AccordionLabelWrapper>
+                            <span>
+                                Разработал парсер железнодорожных рейсов для старого и нового сайтов РЖД,
+                                успешно провел тестирование на совпадение рейсов.
+                            </span>
+                            <Cup title={'Победитель'}/>
+                        </AccordionLabelWrapper>
+                    }>
+                        <AccordionContentWrapper>
+                            <span>Выполненные требования: </span>
+                            <ul tw={'list-disc ms-5'}>
+                                <li>формирование таблицы excel извлеченным данным;</li>
+                                <li>анализ результатов тестирования и вывод в таблицу;</li>
+                                <li>выделение ошибочных результатов в таблице.</li>
+                            </ul>
+                        </AccordionContentWrapper>
+                    </Accordion>
+                </CompanyWrapper>
             </TableContent>
         </tr>
     );
 };
 
 
-const CompanyWrapper = ({children}: { children: ReactNode }) => (
-    <div className={'pb-5'}>
-        {children}
-    </div>
-)
 const TitleWrapper = ({title, children}: { title: string, children: ReactNode }) => (
-    <div className={'flex flex-wrap justify-between'}>
-        <span className={'font-medium text-2xl'}>{title}</span>
+    <div tw={'flex flex-wrap justify-between'}>
+        <span tw={'font-medium text-2xl'}>{title}</span>
         {children}
     </div>
 )
-const ItemContainer = ({children}: { children: ReactNode[] }) => (
-    <div className='flex flex-wrap gap-x-3 gap-y-1 mt-2'>
-        {children}
-    </div>
-)
-
 const Cup = ({title}: { title: string }) => (
-    <div className={'flex gap-2 items-center'}>
-        <TfiCup color={''}/>
-        <span className={'text-lg'}>{title}</span>
+    <div tw={'flex gap-2 items-center'}>
+        <TfiCup/>
+        <span tw={'text-lg'}>{title}</span>
     </div>
 )
-const AccordionLabelWrapper = ({children}: { children: ReactNode[] }) => (
-    <div className={'max-w-[800px]'}>
-        {children}
-    </div>
-)
-const AccordionContentWrapper = ({children}: { children: ReactNode[] }) => (
-    <div className={'py-3'}>
-        {children}
-    </div>
-)
-
-const HackathonWebant = () => (
-    <CompanyWrapper>
-        <TitleWrapper title={'День карьеры ДГТУ 2023 - Вебант'}>
-            <SimpleDate date={<time dateTime={'2021-03'}>Март 2023</time>}/>
-        </TitleWrapper>
-        <ItemContainer>
-            <Item label={'React'}/>
-            <Item label={'NodeJS'}/>
-            <Item label={'ExpressJS'}/>
-            <Item label={'PostgreSQL'}/>
-            <Item label={'Chrome extension'}/>
-        </ItemContainer>
-        <Accordion
-            label={
-                <AccordionLabelWrapper>
-                    <span className={''}>
-                                        Разработал веб-приложение для парсинка резюме с сайта <a href={'https://hh.ru'}
-                                                                                                 className='text-blue-400 underline'>hh.ru</a>
-                                        , реализовал дополнительные возможности по улучшению пользовательского опыта.
-                    </span>
-                    <Cup title={'Победитель'}/>
-                </AccordionLabelWrapper>
-            }
-        >
-            <AccordionContentWrapper>
-                <span className={''}>Выполненные требования: </span>
-                <ul className={'list-disc ms-5'}>
-                    <li>парсинг основных атрибутов по ссылке на резюме;</li>
-                    <li>сохранение в базу данных после проверки пользователем;</li>
-                    <li>веб-интерфейс для CRUD операций над резюме.</li>
-                </ul>
-                <span className={''}>Дополнительно реализовано: </span>
-                <ul className={'list-none ms-5'}>
-                    <li>
-                        <BsFire className={'inline'}/>
-                        chrome extension content script - встраивает в страницу резюме кнопку для парсинга
-                    </li>
-                    <li>
-                        <BsFire className={'inline'}/>
-                        chrome extension popup script - добавляет в браузер popup окно с возможностью моментального
-                        парсинга прямо из браузера
-                    </li>
-                </ul>
-            </AccordionContentWrapper>
-        </Accordion>
-    </CompanyWrapper>
-)
-
-
-const HackathonRostelecom = () => (
-    <CompanyWrapper>
-        <TitleWrapper title={'Хакатон 2023 - Ростелеком'}>
-            <SimpleDate date={<time dateTime={'2021-03'}>Март 2023</time>}/>
-        </TitleWrapper>
-        <ItemContainer>
-            <Item label={'React'}/>
-            <Item label={'XLSX'}/>
-        </ItemContainer>
-        <Accordion label={
-            <AccordionLabelWrapper>
-                <span>
-                    Разработал веб-приложение для работы с таблицами Excel внутри браузера.
-                </span>
-                <Cup title={'Финалист'}/>
-            </AccordionLabelWrapper>
-        }>
-            <AccordionContentWrapper>
-                <span>Выполненные требования: </span>
-                <ul className={'list-disc ms-5'}>
-                    <li>поиск заявок на подключение к сети по имени или ИНН клиента;</li>
-                    <li>обнаружение "запоздавших" по времени исполнения заявок (SLA);</li>
-                    <li>программная реализация в виде веб-приложения;</li>
-                    <li>возможность работы в оффлайн;</li>
-                    <li>дальнейшая возможность подключения к внутреннему REST API компании.</li>
-                </ul>
-            </AccordionContentWrapper>
-        </Accordion>
-    </CompanyWrapper>
-)
-
-
-const HackathonTTK = () => (
-    <CompanyWrapper>
-        <TitleWrapper title={'Хакатон 2021 - ТТК'}>
-            <SimpleDate date={<time dateTime={'2021-03'}>Март 2021</time>}/>
-        </TitleWrapper>
-        <ItemContainer>
-            <Item label={'Python'}/>
-            <Item label={'Selenium'}/>
-            <Item label={'PyQT5'}/>
-        </ItemContainer>
-        <Accordion label={
-            <AccordionLabelWrapper>
-                <span>
-                    Разработал парсер железнодорожных рейсов для старого и нового сайтов РЖД,
-                    успешно провел тестирование на совпадение рейсов.
-                </span>
-                <Cup title={'Победитель'}/>
-            </AccordionLabelWrapper>
-        }>
-            <AccordionContentWrapper>
-                <span className={''}>Выполненные требования: </span>
-                <ul className={'list-disc ms-5'}>
-                    <li>формирование таблицы excel извлеченным данным;</li>
-                    <li>анализ результатов тестирования и вывод в таблицу;</li>
-                    <li>выделение ошибочных результатов в таблице.</li>
-                </ul>
-            </AccordionContentWrapper>
-        </Accordion>
-    </CompanyWrapper>
-)
-
+const CompanyWrapper = tw.div`pb-5`
+const ItemContainer = tw.div`flex flex-wrap gap-x-3 gap-y-1 mt-2`
+const AccordionLabelWrapper = tw.div`max-w-[800px]`
+const AccordionContentWrapper = tw.div`py-3`
+const SimpleDate = tw.div`flex gap-1 items-center`
 
 export default Experience;
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,8 +1,7 @@
-export const Item = ({label}: { label: string }) => (
-    <div
-        className={"transition-colors w-min-[40px] px-1.5 py-0.5 bg-[#f1f2f9] hover:bg-[#e6e2ea] dark:bg-slate-700 dark:hover:bg-slate-600 rounded-lg text-center "}>
-        <span className={"text-[#64748b] dark:text-slate-300 font-medium overflow-hidden overflow-ellipsis line-clamp-2"}>
-            {label}
-        </span>
-    </div>
-)
+import tw from "twin.macro";
+import styled from "@emotion/styled";
+
+export const Item = styled.div([
+    tw`transition-colors min-w-[40px] px-1.5 py-0.5 rounded-lg text-center font-medium overflow-hidden overflow-ellipsis line-clamp-2`,
+    tw`text-[#64748b] bg-[#f1f2f9] hover:(bg-[#e6e2ea] dark:bg-slate-600) dark:(bg-slate-700 text-slate-300)`
+])
